7b3b580d7efda97e844d98f15e7e99b1b3efee87
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-01-05 13:50:06 -0600

    k

--- kitbot1.py
+++ kitbot1.py
@@ -1,22 +1,22 @@
 import RoboPiLib as RPL
 import setup
 import time
-def cease:
+def cease():
     exit(0)
 RPL.digitalWrite(18,1)
 RPL.digitalWrite(20,0)
 RPL.servowrite(1,0)
 RPL.servowrite(0,0)
-def stop:
+def stop():
     RPL.servowrite(0,0)
     RPL.servowrite(1,0)
-def hard_turn_left:
+def hard_turn_left():
     rpl.servowrite(0,1500)
-def turn_left:
+def turn_left():
     rpl.servowrite(0,1000)
-def turn_right:
+def turn_right():
     rpl.servowrite(1,1000)
-def hard_turn_right:
+def hard_turn_right():
     rpl.servowrite(1,1500)
 if rpl.readDistance(1) < 30:
     turn_left()
